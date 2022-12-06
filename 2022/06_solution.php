afb8aa5bea16e482252e5fb05fd0f97716fcc445
Joakim Ö <10329253+joakimbo@users.noreply.github.com>
2022-12-06 07:46:10 +0100

    06 solution

---
+++ 2022/06_solution.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+$input = trim(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . "06_input.txt"));
+
+function tests()
+{
+    $inputs = [
+        'partOne' => [
+            'length' => 4,
+            'inputs' => [
+                'mjqjpqmgbljsphdztnvjfqwrcgsmlb'    => 7,
+                'bvwbjplbgvbhsrlpgdmjqwftvncz'      => 5,
+                'nppdvjthqldpwncqszvftbrmjlhg'      => 6,
+                'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg' => 10,
+                'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'  => 11,
+            ],
+        ],
+        'partTwo' => [
+            'length' => 14,
+            'inputs' => [
+                'mjqjpqmgbljsphdztnvjfqwrcgsmlb'    => 19,
+                'bvwbjplbgvbhsrlpgdmjqwftvncz'      => 23,
+                'nppdvjthqldpwncqszvftbrmjlhg'      => 23,
+                'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg' => 29,
+                'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'  => 26,
+            ]
+        ],
+    ];
+    foreach ($inputs as $part) {
+        foreach ($part['inputs'] as $testInput => $expectedValue) {
+            $result = getCharCountBeforeStartPos($testInput, $part['length']);
+            if ($result != $expectedValue) {
+                echo "Error, expected {$expectedValue} got {$result}\n";
+            }
+        }
+    }
+}
+
+function getCharCountBeforeStartPos(string $input, $length = 4): int
+{
+    $inputLength = strlen($input);
+    for ($i = 0; $i < $inputLength; $i++) {
+        $temp = substr($input, $i, $length);
+        if (count(array_unique(str_split($temp))) == $length) {
+            return $i+$length;
+        }
+    }
+    return -1;
+}
+
+tests();
+
+echo "Part one: " . getCharCountBeforeStartPos($input) . "\n"; // 1582
+echo "Part two: " . getCharCountBeforeStartPos($input, 14) . "\n"; // 3588
